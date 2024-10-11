a69e4c90768437147e603183c7c62792aec79a84
Nicolas Mello <127401029+NicolasMellouz@users.noreply.github.com>
2024-10-11 10:56:47 -0300

    2.0

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-
-import { fastify } from 'fastify'
-import cors from '@fastify/cors'
-import { DatabasePostgres } from './database-postgres.js'
-import { DatabaseProdutos } from './database.js';
+import { fastify } from 'fastify';
+import cors from '@fastify/cors';
+import { DatabasePostgres } from './database-postgres.js';
 
 const server = fastify();
-const database = new DatabaseProdutos;
+const databasePostgres = new DatabasePostgres;
 
 // CORS
 server.register(cors, {
@@ -18,26 +16,108 @@ server.register(cors, {
 // CREATE
 server.post('/produtos', async (request, reply) => {
     const body = request.body;
-    await database.createProduto(body);
-    return 201
+    let error = {};
+
+    if (!body.name) {
+        error.name = 'O Name não foi Informado'
+    } 
+    
+    if (!body.descricao) {
+        error.descricao = 'A Descricao não foi Informada'
+    } 
+
+    if(body.name && body.descricao) {
+        await databasePostgres.createProduto(body);
+        return reply.status(201).send();
+    } else {
+        return reply.status(400).send(error);
+    }
+
+    // await databasePostgres.createProduto(body);
+    // return reply.status(201).send();
 })
 
-
 // READ
-server.get('/', () => {
-    return 'Mamma';
+server.get('/produtos', async () => {
+    const produtos = await databasePostgres.listProdutos();
+    return produtos;
 });
 
-server.get('/produtos', () => {
-    return 'Mamma spaghetti';
-});
+// // UPDATE
+// server.put('/produtos/:id', async (request, reply) => {
+//     const produtoID = request.params.id;
+//     const body = request.body;
+//     await databasePostgres.updateProduto(produtoID, body);
 
+//     let error = {};
+
+//     if(!produtoID) {
+//         error.id = 'O valor não foi informado'
+//     }
+
+//     if (!body.name) {
+//         error.name = 'O Name não foi Informado'
+//     } 
+    
+//     if (!body.descricao) {
+//         error.descricao = 'A Descricao não foi Informada'
+//     } 
+
+//     if(body.name && body.descricao && produtoID) {
+//         await databasePostgres.updateProduto(produtoID, body);
+//         return reply.status(204).send('Alterado com sucesso');
+//     } else {
+//         return reply.status(400).send(error);
+//     }
+
+// })
 
 // UPDATE
+server.put('/produtos/:id', async (request, reply) => {
+    const produtoID = request.params.id;
+    const body = request.body;
+    let error = {};
 
+    // Validação do ID
+    if (!produtoID) {
+        error.id = 'O ID do produto não foi informado';
+    }
+
+    // Validação dos campos
+    if (!body.name) {
+        error.name = 'O Name não foi Informado';
+    }
+    
+    if (!body.descricao) {
+        error.descricao = 'A Descricao não foi Informada';
+    }
+
+    // Se houver erros, retornar 400 com as mensagens
+    if (Object.keys(error).length > 0) {
+        return reply.status(400).send(error);
+    }
+
+    // Se todos os dados forem válidos, atualiza o produto
+    try {
+        const updatedProduto = await databasePostgres.updateProduto(produtoID, body);
+        
+        if (updatedProduto) {
+            return reply.status(204).send('Alterado com sucesso');
+        } else {
+            return reply.status(404).send({ error: 'Produto não encontrado' });
+        }
+    } catch (err) {
+        return reply.status(500).send({ error: 'Erro ao atualizar o produto' });
+    }
+});
 
 // DELETE
+server.delete('/produtos/:id', async (request, reply) => {
+    const produtoID = request.params.id;
+    await databasePostgres.deleteProduto(produtoID);
 
+    return reply.status(204).send();
+})
 
 server.listen({
     port: 3333
